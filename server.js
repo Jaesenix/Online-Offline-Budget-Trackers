d91e9aa8c58e361526e8cc287b9596d2ef56e73b
Janella Senica <janella.senica@gmail.com>
2020-08-11 21:05:58 -0500

    rev server.js

--- server.js
+++ server.js
@@ -15,11 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://juser:dbpassword@ds151066.mlab.com:51066/heroku_zkn9x2l2", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+
+mongoose.connect(MONGODB_URI);
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
 
 // routes
 app.use(require("./routes/api.js"));

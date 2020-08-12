43b04a613faded16a8c8aca1c0a98ac286c7f65e
Janella Senica <janella.senica@gmail.com>
2020-08-11 22:08:33 -0500

    rev mongoose connect

--- server.js
+++ server.js
@@ -18,11 +18,6 @@ app.use(express.static("public"));
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 mongoose.connect(MONGODB_URI);
-// mongoose.connect(
-//   process.env.MONGODB_URI || "mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
 
 // routes
 app.use(require("./routes/api.js"));
